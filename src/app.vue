8bb5920277b40317ef658729843d944ec0b362cd
daiwenwen399 <944148423@qq.com>
2020-11-27 16:59:56 +0800

    feat:创建路由

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>app</div>
+    <div>
+        <Header />
+        <router-view></router-view>
+        <Footer />
+    </div>
 </template>
 
 <script>
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 export default {
-name:"App"
-}
+    name: "App",
+    components: {
+        Header,
+        Footer,
+    },
+};
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './app.vue'
 
+import router from './router'
+
+Vue.config.productionTip = false
+
 new Vue({
     render: h => h(App),
+    // 应用路由
+    router,
 }).$mount("#app")
