4823cbb8cb3e3e421ff42cdb7cf8cab27d16672b
daiwenwen399 <944148423@qq.com>
2020-11-28 11:51:47 +0800

    feat:重写push\replace方法解决重复点击报错问题

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,28 @@ import Login from '../views/Login'
 import Register from '../views/Register'
 import Search from '../views/Search'
 
+// 重写push\replace方法解决报错问题
+const newPush = VueRouter.prototype.push;
+const newReplace = VueRouter.prototype.replace;
+
+VueRouter.prototype.push = function (location, oncomplete, onAbort) {
+    // 如果想处理失败就处理
+    if (oncomplete && onAbort) {
+        return newPush.call(this, location, oncomplete, onAbort)
+    }
+    // 如果不想处理就设置为空函数
+    return newPush.call(this, location, oncomplete, () => { })
+}
+
+VueRouter.prototype.replace = function (location, oncomplete, onAbort) {
+    // 如果想处理失败就处理
+    if (oncomplete && onAbort) {
+        return newReplace.call(this, location, oncomplete, onAbort)
+    }
+    // 如果不想处理就设置为空函数
+    return newReplace.call(this, location, oncomplete, () => { })
+}
+
 // 使用插件
 Vue.use(VueRouter)
 
