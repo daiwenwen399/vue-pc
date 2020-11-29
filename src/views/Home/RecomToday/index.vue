d17b15a976b6d1445773bc2af4c6e7da3b694ba0
daiwenwen399 <944148423@qq.com>
2020-11-29 22:08:09 +0800

    feat:完成今日推荐静态页面

---
+++ src/views/Home/RecomToday/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- 今日推荐区 -->
+    <div id="recom">
+        <div class="recom_title">
+            <img src="./images/clock.png" />
+            <h3>今日推荐</h3>
+        </div>
+        <div class="recom_goods">
+            <ul>
+                <li><img src="./images/today01.png" /></li>
+                <li><img src="./images/today02.png" /></li>
+                <li><img src="./images/today03.png" /></li>
+                <li><img src="./images/today04.png" /></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "RecomToday",
+};
+</script>
+
+<style lang="less" scoped>
+#recom {
+    width: 1200px;
+    margin: 10px auto;
+    display: flex;
+    .recom_title {
+        width: 205px;
+        height: 163px;
+        text-align: center;
+        background-color: #5c5251;
+        img {
+            margin-top: 30px;
+        }
+        h3 {
+            line-height: 30px;
+            font-size: 18px;
+            font-weight: 700;
+            color: #fff;
+            margin: 9px 0;
+        }
+    }
+    ul {
+        display: flex;
+    }
+    li:hover {
+        opacity: 0.8;
+        transition: linear 400ms;
+    }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,18 +2,21 @@
     <div>
         <ClassifiedNav />
         <Banner />
+        <RecomToday />
     </div>
 </template>
 
 <script>
 import ClassifiedNav from "./ClassifiedNav";
 import Banner from "./Banner";
+import RecomToday from "./RecomToday";
 
 export default {
     name: "Home",
     components: {
         ClassifiedNav,
         Banner,
+        RecomToday,
     },
 };
 </script>
