0dbeaff316895b17316acc7ce3837bc16493abcc
daiwenwen399 <944148423@qq.com>
2020-11-30 23:10:06 +0800

    feat:完成登录页面底部版权区

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -46,7 +46,20 @@
                 </div>
             </div>
         </div>
-        <div class="login_copyright"></div>
+        <div class="login_copyright">
+            <ul>
+                <li><a>关于我们</a></li>
+                <li><a>联系我们</a></li>
+                <li><a>人才招聘</a></li>
+                <li><a>商家入驻</a></li>
+                <li><a>广告服务</a></li>
+                <li><a>手机京东</a></li>
+                <li><a>友情链接</a></li>
+                <li><a>销售联盟</a></li>
+                <li class="last"><a>京东社区</a></li>
+            </ul>
+            <p>Copyright©2004-2020 尚品汇shang.com 版权所有</p>
+        </div>
     </div>
 </template>
 
@@ -215,6 +228,32 @@ export default {
                 color: #e1251b;
                 font-size: 16px;
             }
+        }
+    }
+    .login_copyright {
+        width: 1200px;
+        border-top: 1px solid #ccc;
+        margin: 0 auto;
+        padding: 30px;
+        text-align: center;
+        ul {
+            width: 640px;
+            height: 30px;
+            line-height: 30px;
+            margin: 0 auto;
+            display: flex;
+        }
+        li::after {
+            content: "|";
+            padding: 0 10px;
+        }
+        .last::after {
+            content: "";
+        }
+        p {
+            margin-top: 20px;
+            height: 30px;
+            line-height: 30px;
         }
     }
 }
